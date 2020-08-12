79372ec592427de6109984c01173b61fa6666360
Alex <alexisperezmnto@gmail.com>
2020-08-12 11:44:43 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+To run back:
+
+Set Database credentials (user, password, db) in: config/helpers.js file
+
+npm i
+npm i nodemon
+npm start
+
---
+++ routes/products.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const router = express.Router();
+const {database} = require('../config/helpers');
+
+/* GET ALL PRODUCTS */
+router.get('/', function (req, res) { 
+    
+    let sort = {}
+    if(req.query.sort !== undefined && req.query.sort !== '') {
+        
+        if(req.query.sort === 'price') {
+            sort = {price: 1} 
+        } else if(req.query.sort === 'recent') {
+            sort = {id: -1} 
+        } else {
+            sort = {name: 1}
+        }
+        
+    } else {
+        sort = {name: 1}
+    }
+
+    let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;
+    const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10;
+    let startValue;
+    let endValue;
+    if (page > 0) {
+        startValue = (page * limit) - limit;     
+        endValue = page * limit;
+    } else {
+        startValue = 0;
+        endValue = 10;
+    }
+    database.table('products as p')
+        .join([
+            {
+                table: "categories as c",
+                on: `c.id = p.cat_id`
+            }
+        ])
+        .withFields(['c.title as category',
+            'p.title as name',
+            'p.price',
+            'p.quantity',
+            'p.description',
+            'p.image',
+            'p.id'
+        ])
+        // .slice(startValue, endValue)
+        .sort(sort)
+        .getAll()
+        .then(prods => {
+            if (prods.length > 0) {
+                res.status(200).json({
+                    count: prods.length,
+                    products: prods
+                });
+            } else {
+                res.json({message: "No products found"});
+            }
+        })
+        .catch(err => console.log(err));
+});
+
+/* GET ONE PRODUCT*/
+router.get('/:prodId', (req, res) => {
+    let productId = req.params.prodId;
+    database.table('products as p')
+        .join([
+            {
+                table: "categories as c",
+                on: `c.id = p.cat_id`
+            }
+        ])
+        .withFields(['c.title as category',
+            'p.title as name',
+            'p.price',
+            'p.quantity',
+            'p.description',
+            'p.image',
+            'p.id',
+            'p.images'
+        ])
+        .filter({'p.id': productId})
+        .get()
+        .then(prod => {
+            console.log(prod);
+            if (prod) {
+                res.status(200).json(prod);
+            } else {
+                res.json({message: `No product found with id ${productId}`});
+            }
+        }).catch(err => res.json(err));
+});
+
+/* GET ALL PRODUCTS FROM ONE CATEGORY */
+router.get('/category/:catName', (req, res) => { 
+    let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;   
+    const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10;   
+    let startValue;
+    let endValue;
+    if (page > 0) {
+        startValue = (page * limit) - limit;      
+        endValue = page * limit;                  
+    } else {
+        startValue = 0;
+        endValue = 10;
+    }
+
+    // Get category title value from param
+    const cat_title = req.params.catName;
+
+    database.table('products as p')
+        .join([
+            {
+                table: "categories as c",
+                on: `c.id = p.cat_id WHERE c.title LIKE '%${cat_title}%'`
+            }
+        ])
+        .withFields(['c.title as category',
+            'p.title as name',
+            'p.price',
+            'p.quantity',
+            'p.description',
+            'p.image',
+            'p.id'
+        ])
+        .slice(startValue, endValue)
+        .sort({id: 1})
+        .getAll()
+        .then(prods => {
+            if (prods.length > 0) {
+                res.status(200).json({
+                    count: prods.length,
+                    products: prods
+                });
+            } else {
+                res.json({message: `No products found matching the category ${cat_title}`});
+            }
+        }).catch(err => res.json(err));
+
+});
+
+
+module.exports = router;
